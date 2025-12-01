04435d3211b21aa6c51eff718b9988a12678430e
Kavish Nag <kavishnag13@gmail.com>
2025-12-01 23:15:32 +0530

    Create DAY 1.sql

---
+++ DAY 1.sql
@@ -0,0 +1,5 @@
+--Ques: 
+--Reindeer Run - Find the Top 7 Every year, the city of Whoville conducts a Reindeer Run to find the best reindeers for Santa's Sleigh. 
+--Write a query to return the name and rank of the top 7 reindeers in this race.
+
+SELECT name,rank FROM reindeer_run_results ORDER BY rank LIMIT 7;
