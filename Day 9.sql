7ba7248a1d75c7450b3ee60b61c4c25c0003b4e8
Kavish Nag <kavishnag13@gmail.com>
2025-12-12 00:38:18 +0530

    Create Day 9.sql

---
+++ Day 9.sql
@@ -0,0 +1,5 @@
+-- Tinsel and Light Combinations
+-- The elves are testing new tinsel–light combinations to find the next big holiday trend. 
+-- Write a query to generate every possible pairing of tinsel colors and light colors, include in your output a column that combines the two values separated with a dash ("-").
+
+SELECT t.color_name AS tinsel_color,l.color_name AS light_color, t.color_name || '-' || l.color_name FROM tinsel_colors t CROSS JOIN light_colors l;
